f908633b196982641322fb3990b9dacfce0ca6fb
Abdullah Al Maruf <almaruf@softograph.com>
2025-02-27 12:52:28 +0600

    first commit

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'users'
+        
