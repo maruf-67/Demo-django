0738c2f54545c08cc717ba1fb168017ca227ca1e
Abdullah Al Maruf <almaruf@softograph.com>
2025-03-10 12:28:19 +0600

    register view done using class based method

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     # path('profile/', views.profile, name='profile'),
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate,login as auth_login,logout as auth_logout
 from django.contrib import messages
+from django.views import View
 
 
 def login(request):
@@ -47,3 +48,20 @@ def logout(request):
     auth_logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect('index')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html',{'register_form': register_form})
+
+    def post(self, request):
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            username = register_form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}")
+            auth_login(request, user)
+            return redirect('index')
+        return render(request, 'views/register.html',{'register_form': register_form})
